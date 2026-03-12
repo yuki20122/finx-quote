6d5c155e3eae140766f5b1857793d44cdd714edb
yuki20122 <yuki.2012g@gmail.com>
2026-03-12 23:29:10 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+window.APP_CONFIG = {
+  restOrderbookUrl: "https://okj-proxy.yuki-2012g.workers.dev/okj/orderbook",
+  okjTickerUrl: "https://okj-proxy.yuki-2012g.workers.dev/okj/ticker",
+  bittradeOrderbookUrl: "https://okj-proxy.yuki-2012g.workers.dev/bittrade/orderbook",
+  b2c2PriceUrl: "http://127.0.0.1:8787/b2c2/prices",
+  b2c2ApiToken: "",
+  b2c2BtcQty: 0.5,
+  b2c2EthQty: 25,
+  diffHighlightPct: 0.2,
+  tradingDaysPerYear: 243,
+  pollIntervalMs: 5000,
+  instruments: ["BTC-JPY", "ETH-JPY"],
+};
